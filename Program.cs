ccbff9edcf156096c7991cf5a8f8ccd8e771c7a9
Renato Netto <rnetto94@live.com>
2021-06-29 13:34:11 -0300

    start commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsultaViaCep_HTTPClient
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+            
+        static async Task Main(string[] args)
+        {
+            await ProcessaRepositorio();
+        }
+
+        private static async Task ProcessaRepositorio()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositorios = await JsonSerializer.DeserializeAsync<List<Repositorio>>(await streamTask);
+
+            foreach (var rep in repositorios)
+            {
+                Console.WriteLine(rep.MostraRepositorio());
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Repositorio.cs
@@ -0,0 +1,29 @@
+﻿
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace ConsultaViaCep_HTTPClient
+{
+    public class Repositorio
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Nome { get; set; }
+        [JsonPropertyName("private")]
+        public bool Privado { get; set; }
+        [JsonPropertyName("html_url")]
+        public string Url { get; set; }
+        [JsonPropertyName("description")]
+        public string Descricao { get; set; }
+
+        public string MostraRepositorio()
+        {
+            return "ID: " + Id
+                + "\nNome: " + Nome
+                + "\nPrivado: " + Privado
+                + "\nURL: " + Url
+                + "\nDiscrição: " + Descricao;
+        }
+    }
+}
